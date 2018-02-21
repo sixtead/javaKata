09b2542b0eadcf96d147d3760e4bfb196f30d5fd
sixtead <sixtead@gmail.com>
2018-02-21 14:13:48 +0300

    Solved 6kyu TripleTrouble

---
+++ src/main/java/com/codewars/sixtead/kyu6/TripleTrouble.java
@@ -0,0 +1,22 @@
+package com.codewars.sixtead.kyu6;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TripleTrouble {
+
+    public static int TripleDouble(long num1, long num2) {
+        Pattern p1 = Pattern.compile("(\\d)\\1\\1");
+        Matcher m1 = p1.matcher(String.valueOf(num1));
+        Pattern p2 = Pattern.compile("(\\d)\\1");
+        Matcher m2 = p2.matcher(String.valueOf(num2));
+
+        System.out.println(String.valueOf(num1));
+        while (m1.find()) {
+            while(m2.find()) {
+                if(m1.group().substring(0, 2).equals(m2.group())) return 1;
+            }
+        }
+        return 0;
+  }
+}
---
+++ src/test/java/com/codewars/sixtead/kyu6/TripleTroubleTest.java
@@ -0,0 +1,32 @@
+package com.codewars.sixtead.kyu6;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TripleTroubleTest {
+    @Test
+    public void test1(){
+      assertEquals(1, TripleTrouble.TripleDouble(451999277L, 41177722899L));
+    }
+
+    @Test
+    public void test2(){
+      assertEquals(0, TripleTrouble.TripleDouble(1222345L, 12345L));    
+    }
+    
+    @Test
+    public void test3(){
+      assertEquals(0, TripleTrouble.TripleDouble(12345L, 12345L));
+    }
+    
+    @Test
+    public void test4(){
+      assertEquals(1, TripleTrouble.TripleDouble(666789L, 12345667L));
+    }
+    
+    @Test
+    public void test5() {
+      assertEquals(0, TripleTrouble.TripleDouble(451999277, 411777228L));
+    }
+}
