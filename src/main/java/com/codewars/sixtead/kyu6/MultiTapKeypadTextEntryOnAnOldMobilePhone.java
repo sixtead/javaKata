5c876c9bd174d4504e01a2192167e4dc63131d83
sixtead <sixtead@gmail.com>
2018-02-21 15:20:09 +0300

    Completed 6Kyu MultiTapKeypadTextEntryOnAnOldMobilePhone

---
+++ src/main/java/com/codewars/sixtead/kyu6/MultiTapKeypadTextEntryOnAnOldMobilePhone.java
@@ -0,0 +1,20 @@
+package com.codewars.sixtead.kyu6;
+
+public class MultiTapKeypadTextEntryOnAnOldMobilePhone {
+    private static final String[] keypad;
+    static {
+        keypad = new String[] {"1", "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", "PQRS7", "TUV8", "WXYZ9", "*", " 0", "#"};
+            }
+
+    public static int presses(String phrase) {
+        int sum = 0;
+
+        for(String symbol : phrase.toUpperCase().split("")) {
+            for(String key : keypad) {
+                sum += key.indexOf(symbol) + 1;
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ src/test/java/com/codewars/sixtead/kyu6/MultiTapKeypadTextEntryOnAnOldMobilePhoneTest.java
@@ -0,0 +1,13 @@
+package com.codewars.sixtead.kyu6;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MultiTapKeypadTextEntryOnAnOldMobilePhoneTest {
+    @Test
+    public void simpleTest() {
+      assertEquals(9, MultiTapKeypadTextEntryOnAnOldMobilePhone.presses("LOL"));
+      assertEquals(13, MultiTapKeypadTextEntryOnAnOldMobilePhone.presses("HOW R U"));
+    }
+}
