79fa15311eefe475b61c98aa003402f9167abfc3
sixtead <sixtead@gmail.com>
2018-06-19 17:03:03 +0300

    added check for v1 >= v2 in 6kyu:Tortoise racing

--- src/main/java/com/codewars/sixtead/kyu6/TortoiseRacing.java
+++ src/main/java/com/codewars/sixtead/kyu6/TortoiseRacing.java
@@ -3,6 +3,9 @@ package com.codewars.sixtead.kyu6;
 public class TortoiseRacing {
 
     public static int[] race(int v1, int v2, int g) {
+
+        if(v1 >= v2) return null;
+        
         int time;
         int hours;
         int minutes;
